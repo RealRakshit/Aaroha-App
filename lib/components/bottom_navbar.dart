4666595606c2968781c398d268ff6721ce9308ab
Nishant sharma <nishantsharma7132@gmail.com>
2024-12-21 23:59:17 +0530

    Add files via upload

---
+++ lib/components/bottom_navbar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../components/constants.dart';
+
+class CustomBottomBar extends StatefulWidget {
+  const CustomBottomBar({super.key});
+
+  @override
+  State<CustomBottomBar> createState() => _CustomBottomBarState();
+}
+
+class _CustomBottomBarState extends State<CustomBottomBar> {
+  int myIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height:75.0,
+      child:ClipRRect(
+      
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(30), // Rounded upper-left corner
+        topRight: Radius.circular(30), // Rounded upper-right corner
+      ),
+      child: BottomNavigationBar(
+        
+        backgroundColor: color1,
+        selectedItemColor: Colors.white, // Color for selected item
+        unselectedItemColor: const Color.fromARGB(145, 255, 255, 255), // Color for unselected items
+        type: BottomNavigationBarType.fixed, 
+        currentIndex: myIndex,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined, size:35), label: 'Home',),
+          BottomNavigationBarItem(icon: Icon(Icons.folder_copy_outlined,size:35), label: 'Projects'),
+          BottomNavigationBarItem(icon: Icon(Icons.event_available_outlined,size:35), label: 'Events'),
+          BottomNavigationBarItem(icon: Icon(Icons.volunteer_activism_outlined,size:35), label: 'Donate'),
+        ],
+      ),
+    ),);
+  }
+}
---
+++ lib/components/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color background_Color=Color(0xFFCCEAF0);
+const Color log_Out=Color(0xFF812204);
+const Color color1=Color(0xFF53B5C6);
+const Color color2=Color(0xFF187B9D);
+const Color color3=Color(0xFF75C4D1);
+const Color color4=Color(0xFF2EF2F5);
+
